b600ea979ffc47010f6aa29e5b0936ee456c6731
j4ir0st <jairocast2@gmail.com>
2021-12-08 14:02:26 -0800

    setting to exit 0

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv, char **env)
 			if (cmd.band == 1)
 				_free(cmd.env);
 			free(buffer);
-			exit(98);
+			exit(0);
 		}
 		bypass(buffer, argv, &cmd);
 		if (a == 0)
